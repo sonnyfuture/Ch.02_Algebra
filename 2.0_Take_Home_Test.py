f61d73a4d79036aa7969edf2ac982401ca1c12ee
agicedd <agicedd@urbandaleschools.com>
2018-12-11 13:17:07 -0600

    Sold my soul to the devil

--- 2.0_Take_Home_Test.py
+++ 2.0_Take_Home_Test.py
@@ -1,6 +1,6 @@
 '''
 HONOR CODE: I solemnly promise that while taking this test I will only use PyCharm or the Internet,
-but I will definitely not ask another person except the instructor. Signed: ______________________
+but I will definitely not ask another person except the instructor. Signed: Eddie H. Agic
 
 1.) How do you enter a single line comment in a program?
 
